96547fb9c6622c1eb381ed3a5080a378adb22eae
unknown <vuthanhnguyen2807@gmail.com>
2026-03-24 13:33:44 +0700

    Nguyen update restaurant

---
+++ Assets/Scripts/Player/CameraCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    public Transform target; // player (điểm gốc)
+    public float maxDistance = 0.4f; // khoảng cách cam
+    public float minDistance = 0.1f;
+    public LayerMask collisionMask;
+
+    void LateUpdate()
+    {
+        Vector3 dir = (transform.localPosition).normalized;
+
+        RaycastHit hit;
+        if (Physics.Raycast(target.position, target.forward, out hit, maxDistance, collisionMask))
+        {
+            float dist = hit.distance - 0.05f;
+            dist = Mathf.Clamp(dist, minDistance, maxDistance);
+            transform.localPosition = new Vector3(0, 0, dist);
+        }
+        else
+        {
+            transform.localPosition = new Vector3(0, 0, maxDistance);
+        }
+    }
+}
