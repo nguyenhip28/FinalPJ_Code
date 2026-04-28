40db1422704a9f21d320fb81e495d5df39282994
unknown <vuthanhnguyen2807@gmail.com>
2026-04-28 23:15:54 +0700

    Nguyen build game

---
+++ Assets/Editor/PBS Materials Variety Pack/zzz/MaterialRandomizer.cs
@@ -0,0 +1,28 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MaterialRandomizerScript))]
+public class MaterialRandomizer : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        MaterialRandomizerScript myScript = (MaterialRandomizerScript)target;
+        if (GUILayout.Button("Find Materials"))
+        {
+            myScript.findMaterials();
+        }
+        if (GUILayout.Button("Find Spheres"))
+        {
+            myScript.findMaterialSpheres();
+        }
+        if (GUILayout.Button("Randomize Materials"))
+        {
+            myScript.randomizeMaterials();
+        }
+    }
+}
+#endif
